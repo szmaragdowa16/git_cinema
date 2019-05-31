c592193e4c217cfd10e2985ae7cf524cb578636d
szmaragdowa16 <szmaragdowa16@gmail.com>
2019-05-31 13:36:15 +0200

    log toUpperCase

--- logowanie.js
+++ logowanie.js
@@ -1,98 +1,96 @@
-
-
 function validate() {
 
     var mydata = JSON.parse(login);
-        var liczba = Object.keys(mydata).length
-        for (var i = 0; i < liczba; i++) {
-            var name = document.getElementById('name').value
-    
-    
-    
-            var text = 'Wpisz poprawne hasło';
-            var text1 = '';
-            var check = /^(?=.*\d)(?=.*[a-z])(?=.*[\!\@\#\$\%\^\&\*\(\)\_\+\-\=])(?=.*[A-Z])(?!.*\s).{8,}$/;
-    
-    
-            if (check.test(name)) {
-    
-                document.getElementById('test').innerHTML = text1;
-    
-    
-            } else {
-                document.getElementById('test').innerHTML = text;
-                console.log('nie')
-    
-                return false;
-    
-            }
-    
-           
-    
-    
-            var email = document.getElementById('email').value
+    var liczba = Object.keys(mydata).length
+    for (var i = 0; i < liczba; i++) {
+        var name = document.getElementById('name').value
 
-           
-    
-          
-    
-            var text = 'Wpisz poprawny email';
-            var text1 = '';
-            var check = /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$/i
-    
-            if (check.test(email)) {
-                document.getElementById('test').innerHTML = text1;
-    
-            } else  {
-                document.getElementById('test').innerHTML = text;
-                return false;
-            }
-            var strings = [document.getElementById('email').value];
-            function makeUpperCase(v)
-            {
-                return v.toUpperCase();
-            }
-            var uppers = strings.map(makeUpperCase);
-            console.log(uppers)
-            // uppers są teraz pisane wielkimi literami ["HELLO", "ARRAY", "WORLD"]
-            // strings są niezmienione
-    
-    
-            if (mydata[i].password == name && mydata[i].mail == email || email==uppers) {
+// walidacja hasła
+
+        var text = 'Wpisz poprawne hasło';
+        var text1 = '';
+        var check = /^(?=.*\d)(?=.*[a-z])(?=.*[\!\@\#\$\%\^\&\*\(\)\_\+\-\=])(?=.*[A-Z])(?!.*\s).{8,}$/;
+
+
+        if (check.test(name)) {
+
+            document.getElementById('test').innerHTML = text1;
+
+
+        } else {
+            document.getElementById('test').innerHTML = text;
+
+
+            return false;
+
+        }
+
+        //   zamiana input email na duze litery pomocne do logowania niezaleznie od wielkosci liter
+
+
+        var email = document.getElementById('email').value
+        var uperr = email.toUpperCase()
+
+        console.log(uperr)
+
+
+
+        var text = 'Wpisz poprawny email';
+        var text1 = '';
+      
+        var check = /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/
+
+        if (check.test(email)) {
+            document.getElementById('test').innerHTML = text1;
+
+        } else {
+            document.getElementById('test').innerHTML = text;
+            return false;
+
+        }
+
+        // map() maila z json na duze litery, pomocne do logowania niezaleznie od wpisanej wielkosci litery
+        var strings = [mydata[i].mail];
+        var strings = [mydata[i].mail];
+
+        function makeUpperCase(v) {
+            return v.toUpperCase();
+        }
+        var uppers = strings.map(makeUpperCase);
+
+        // uppers są teraz pisane wielkimi literami 
+        // strings są niezmienione
+
+        var password = mydata[i].password
+
+        if (password == name) {
+            if (uperr == uppers) {
                 var img4 = "<img src=emotka.png>"
 
-                
+
                 document.querySelector('.wrapper1').style.visibility = 'hidden'
-               
+
                 document.getElementById('zalogowany').style.visibility = 'visible'
                 document.getElementById('zalogowany').innerHTML = "Zapłać i ciesz się z pobytu w naszym kinie "
                 document.getElementById('log').innerHTML = img4 + "zalogowany"
-                // document.getElementById('zalogowany').style.fontSize='22px';
-               
-               
-                document.getElementById('welcome').style.visibility = 'hidden'
-                document.getElementById('tlo').style.display = 'none'
-            
-    
-    
-                document.getElementById('all').style.visibility = 'visible'
-    
-    
-    
-            }
-            else{
-                var text= "Nie ma Cię w bazie użytkowników,<br> albo wprowadziłeś nieprawidłowy e-mail";
-                document.getElementById('test').innerHTML = text;
-             
-            }
-    
-        }
-        }
-       
-       var znikanie =document.getElementById('button')
-       znikanie.addEventListener('click', function(){
-        // document.getElementById('kup').innerHTML=""
-        document.getElementById('rezerwuje').innerHTML=""
-       })
 
-       
+            } else {
+                var text = "Nie ma Cię w bazie użytkowników,<br> albo wprowadziłeś nieprawidłowe dane";
+                document.getElementById('test').innerHTML = text;
+
+            }
+        } else {
+            var text = "Nie ma Cię w bazie użytkowników,<br> albo wprowadziłeś nieprawidłowe dane";
+            document.getElementById('test').innerHTML = text;
+
+        }
+
+
+    }
+}
+
+var znikanie = document.getElementById('button')
+znikanie.addEventListener('click', function () {
+    // document.getElementById('kup').innerHTML=""
+    document.getElementById('rezerwuje').innerHTML = ""
+})
